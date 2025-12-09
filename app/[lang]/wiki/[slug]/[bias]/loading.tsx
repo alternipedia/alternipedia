70c21641a7d005f5f73223822d261c3dbc55fb38
Jake Walklate <jakewalklate@protonmail.com>
2025-12-10 03:33:18 +1100

    blur

--- app/[lang]/wiki/[slug]/[bias]/loading.tsx
+++ app/[lang]/wiki/[slug]/[bias]/loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/app/(components)/ui/spinner";
 export default function Loading() {
   return (
     <div className="flex items-center justify-center h-[calc(100vh-16rem)] min-h-52">
-      <Spinner className="size-8"/>
+      {/* <Spinner className="size-8"/> */}
     </div>
   );
 }
